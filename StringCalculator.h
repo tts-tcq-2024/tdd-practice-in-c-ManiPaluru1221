77f9a128803cc1eb0e1e29463cc552be05b50e93
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-04 21:07:32 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,55 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define MAX_NUMBER 1000
 
+int Add(const char* numbers);
+void ProcessCustomDelimiter(const char** numbers, char delimiters[][10], int* delimiterCount);
+void SplitNumbers(const char* numbers, char delimiters[][10], int delimiterCount, int* splitNumbers, int* count);
+void ValidateNumbers(int* numbers, int count);
 
-int temp_add(int *pos_arr, const char *inp, char delim, int pos_arr_index){
-    int res = 0;
-    int temp_res = 0;
-    int start;
- //   char *temp = "";
-    
-    if(inp[0] == '/'){
-        start = 1;
-    }
-    else
-        start = 0;
-    for(int i = start ; i < pos_arr_index ; ++i){
-        if((pos_arr[i+1] - pos_arr[i]) >= 5)
-            continue ;
-        temp_res = 0;
-        for(int j = pos_arr[i]+1; j < pos_arr[i+1]; ++j){
-            temp_res = (temp_res * 10) + int(inp[j]);
-        }
-        res += temp_res;
-    }
-    return res;
+int main() {
+    const char* input = "//;\n1;2;1001;3";
+    int result = Add(input);
+    printf("Result: %d\n", result);
+    return 0;
 }
 
-int func(const char *inp, char delim){
-    int pos_arr[strlen(inp)];
-    int index = 0;
-    int pos_arr_index = 0;
-    pos_arr[pos_arr_index] = -1;
-    ++pos_arr_index;
-    while(inp[index] != '\0'){
-        if (inp[index] == delim || inp[index] == '\n'){
-            pos_arr[pos_arr_index] = index;
-            ++pos_arr_index;
+int Add(const char* numbers) {
+    if (numbers == NULL || strlen(numbers) == 0) {
+        return 0;
+    }
+
+    char delimiters[10][10] = { ",", "\n" };
+    int delimiterCount = 2;
+    ProcessCustomDelimiter(&numbers, delimiters, &delimiterCount);
+
+    int splitNumbers[100];
+    int count = 0;
+    SplitNumbers(numbers, delimiters, delimiterCount, splitNumbers, &count);
+    ValidateNumbers(splitNumbers, count);
+
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        if (splitNumbers[i] <= MAX_NUMBER) {
+            sum += splitNumbers[i];
         }
     }
-    pos_arr[pos_arr_index] = strlen(inp);
-    return temp_add(pos_arr, inp, delim, pos_arr_index);
+
+    return sum;
 }
 
-int add(const char *input){
-    int res = 0;
-    if (input == NULL || input[0] == '\0')
-      return 0;
-   if (input[0] == '/'){
-       char delimiter = input[2];
-       res = func(input, delimiter);
-       return res;
-   }
-    res = func(input, ',');
-    return res;
+void ProcessCustomDelimiter(const char** numbers, char delimiters[][10], int* delimiterCount) {
+    if (strncmp(*numbers, "//", 2) == 0) {
+        const char* delimiterEnd = strchr(*numbers, '\n');
+        if (delimiterEnd != NULL) {
+            int length = delimiterEnd - *numbers - 2;
+            strncpy(delimiters[*delimiterCount], *numbers + 2, length);
+            delimiters[*delimiterCount][length] = '\0';
+            (*delimiterCount)++;
+            *numbers = delimiterEnd + 1;
+        }
+    }
+}
+
+void SplitNumbers(const char* numbers, char delimiters[][10], int delimiterCount, int* splitNumbers, int* count) {
+    char temp[1000];
+    strcpy(temp, numbers);
+
+    char* token = strtok(temp, delimiters[0]);
+    for (int i = 1; i < delimiterCount; i++) {
+        token = strtok(NULL, delimiters[i]);
+    }
+
+    while (token != NULL) {
+        splitNumbers[(*count)++] = atoi(token);
+        token = strtok(NULL, delimiters[0]);
+        for (int i = 1; i < delimiterCount; i++) {
+            token = strtok(NULL, delimiters[i]);
+        }
+    }
+}
+
+void ValidateNumbers(int* numbers, int count) {
+    int negativeNumbers[100];
+    int negativeCount = 0;
+
+    for (int i = 0; i < count; i++) {
+        if (numbers[i] < 0) {
+            negativeNumbers[negativeCount++] = numbers[i];
+        }
+    }
+
+    if (negativeCount > 0) {
+        printf("Negatives not allowed: ");
+        for (int i = 0; i < negativeCount; i++) {
+            printf("%d ", negativeNumbers[i]);
+        }
+        printf("\n");
+        exit(1);
+    }
 }
