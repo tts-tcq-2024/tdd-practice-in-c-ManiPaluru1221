be52ec5d0ba683e3f00fb6c28b6f8e1bfa627d5d
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-04 21:17:03 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,12 +5,11 @@
 #define MAX_NUMBER 1000
 
 int add(const char* numbers);
-void ProcessCustomDelimiter(const char** numbers, char delimiters[][10], int* delimiterCount);
-void SplitNumbers(const char* numbers, char delimiters[][10], int delimiterCount, int* splitNumbers, int* count);
+void SplitNumbers(const char* numbers, const char* delimiters, int* splitNumbers, int* count);
 void ValidateNumbers(int* numbers, int count);
 
 int main() {
-    const char* input = "//;\n1;2;1001;3";
+    const char* input = "1,2,1001,3";
     int result = add(input);
     printf("Result: %d\n", result);
     return 0;
@@ -21,13 +20,10 @@ int add(const char* numbers) {
         return 0;
     }
 
-    char delimiters[10][10] = { ",", "\n" };
-    int delimiterCount = 2;
-    ProcessCustomDelimiter(&numbers, delimiters, &delimiterCount);
-
+    const char* delimiters = ",\n";
     int splitNumbers[100];
     int count = 0;
-    SplitNumbers(numbers, delimiters, delimiterCount, splitNumbers, &count);
+    SplitNumbers(numbers, delimiters, splitNumbers, &count);
     ValidateNumbers(splitNumbers, count);
 
     int sum = 0;
@@ -40,55 +36,22 @@ int add(const char* numbers) {
     return sum;
 }
 
-void ProcessCustomDelimiter(const char** numbers, char delimiters[][10], int* delimiterCount) {
-    if (strncmp(*numbers, "//", 2) == 0) {
-        const char* delimiterEnd = strchr(*numbers, '\n');
-        if (delimiterEnd != NULL) {
-            int length = delimiterEnd - *numbers - 2;
-            strncpy(delimiters[*delimiterCount], *numbers + 2, length);
-            delimiters[*delimiterCount][length] = '\0';
-            (*delimiterCount)++;
-            *numbers = delimiterEnd + 1;
-        }
-    }
-}
-
-void SplitNumbers(const char* numbers, char delimiters[][10], int delimiterCount, int* splitNumbers, int* count) {
+void SplitNumbers(const char* numbers, const char* delimiters, int* splitNumbers, int* count) {
     char temp[1000];
     strcpy(temp, numbers);
 
-    char* token = strtok(temp, delimiters[0]);
-    for (int i = 1; i < delimiterCount; i++) {
-        token = strtok(NULL, delimiters[i]);
-    }
-
+    char* token = strtok(temp, delimiters);
     while (token != NULL) {
         splitNumbers[(*count)++] = atoi(token);
-        token = strtok(NULL, delimiters[0]);
-        for (int i = 1; i < delimiterCount; i++) {
-            token = strtok(NULL, delimiters[i]);
-        }
+        token = strtok(NULL, delimiters);
     }
 }
 
 void ValidateNumbers(int* numbers, int count) {
-    int negativeNumbers[100];
-    int negativeCount = 0;
-
     for (int i = 0; i < count; i++) {
         if (numbers[i] < 0) {
-            negativeNumbers[negativeCount++] = numbers[i];
+            printf("Negatives not allowed: %d\n", numbers[i]);
+            exit(1);
         }
-    }
-
-    if (negativeCount > 0) {
-        printf("Negatives not allowed: ");
-        for (int i = 0; i < negativeCount; i++) {
-            printf("%d ", negativeNumbers[i]);
-        }
-        printf("\n");
-        exit(1);
     }
 }
-
- 
